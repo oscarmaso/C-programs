74f26aae0f5de898ddb2fed19f7d1216d8c43628
Oscar Maso Alvarez <oscarmaso22@gmail.com>
2018-01-23 12:57:41 +0100

    mar

--- 10_arrativa/arrabio.cpp
+++ 10_arrativa/arrabio.cpp
@@ -4,6 +4,13 @@
 #define N 3
 int main(){
 
+
+/*
+ *  6 4 1
+ *  5 2 7
+ *  3 8 9
+ *
+ */
     int a[N][N] = {
         {1, 4, 6},
         {7, 2, 5},
@@ -11,7 +18,7 @@ int main(){
     };
     for (int desplaz=0; desplaz<N; desplaz++){
         for (int elem=0; elem<N; elem++)
-            printf("%4i", a[(desplaz+elem)%N][desplaz+elem]%N);
+            printf("%4i", a[elem%N][(desplaz+elem)%N]);
         printf("\n");
 
     }
