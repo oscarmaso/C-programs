1102430eb8deddc0b1736ee08ef26426451ef31c
Oscar Maso Alvarez <oscarmaso22@gmail.com>
2018-02-13 14:27:43 +0100

    update

---
+++ 10_ejer/vector.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Tiene que ser de un Vector
+//Con lo que solo existe columna
+int main(){
+
+    int columna = 0;
+    double nuevo_num;
+    double *vector = NULL;
+
+    printf("Tu vector: \n\n");
+
+
+
+    scanf(" %lf", &nuevo_num);
+
+    vector = (double *) realloc() sizeof(double);
+
+
+    printf("Vector es: %.2lf",nuevo_num);
+
+	return EXIT_SUCCESS;
+}
